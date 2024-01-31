7cdf2cabaa238c6378c67d2843007a18ca6cb62b
Dharmesh Nadola <dharmesh.nadola@nagarro.com>
2024-01-31 23:12:19 +0530

    in environment folder change port to 8007 from 8000

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   envVar: {
     userPoolId: 'ap-southeast-1_6YmSRhy7R',
     userPoolWebClientId: '3kpjpvm1t1v61s9tkf0oi857v3',
-    apiBaseUrl: "http://localhost:8000"
+    apiBaseUrl: "http://localhost:8007"
   }
 };
 
